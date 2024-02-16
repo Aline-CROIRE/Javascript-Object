46774f019c57c41f156405a715de9d4a778cdce0
Aline NIYONIZERA <nikyocroirealine@gmail.com>
2024-02-15 22:44:12 -0800

    These are codes for javascript properties operations

---
+++ ObjectProperties.js
@@ -0,0 +1,13 @@
+//creating object
+let student={
+    name:'alice',
+    age:24,
+    grade:'A',
+};
+console.log(student)//this is to display student object
+//acessing values in object
+let studenAge=student.age;
+console.log(studentAge);
+//updating object values
+student.grade='B';
+console.log(student.grade);
